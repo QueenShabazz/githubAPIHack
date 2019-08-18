c624f50981514da3e4f1b2bda4ec69ba52b78a35
Queen Shabazz <queennefertitishabazz@gmail.com>
2019-08-18 04:02:29 -0700

    finalize double API call

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+async function gitUser(user){
+        let urls =[`https://api.github.com/users/${user}/repos`, `https://api.github.com/users/${user}`] 
+        async function getAllUrls(urls){
+            try {
+                let data = await Promise.all(
+                    urls.map(
+                        url =>
+                            fetch(url)
+                            .then(
+                                (response) => response.json()
+                            )
+                    )
+                );
+                return (data)
+                } catch (error) {
+                    console.log(error)
+                    throw (error)
+                }
+         }
+        var responses = await getAllUrls (urls);
+        //console.log(Object.keys(responses));
+        //displayResults(responses);
+
+        $('#results-list').html(`
+                <li> ${user} has ${responses[0].length} repos: </li>
+                <li><a href=${responses[1].html_url}><img id="prof-pic" src="${responses[1].avatar_url}"/></a></li>
+                <li></li>
+                <li></li>
+        `)
+        let repos = responses[0]
+        let length = responses[0].length
+        for (let i = 0; i < length; i++){
+            $('#results-list').append(`<li><a href="${repos[i].git_url}">${repos[i].name}</a><ul><li>${repos[i].description}</li></ul></li>`)
+        }
+        console.log(responses[0])
+        console.log(responses[1].url);
+        }
+//parallel request 
+
+//submit event listener
+
+/*
+
+let user = $('input').val();
+    let gitName = `https://api.github.com/users/${user}`;
+
+    let process = (prom) =>{
+    prom.then(data=>{
+        let p = document.createElement('p');
+        p.textContent = data.
+    })
+}
+
+*/
+
+
+
+function submit(){
+    $('form').submit(event => {
+    event.preventDefault();
+    let input = $('input').val();
+    gitUser(input);
+    $('#results-list').empty();
+    })
+};
+//function that displays usernames
+
+$(function(){
+    submit();
+
+});
+
+/*responseJson(gitName, function(json){
+            if (json.message === "Not Found" || user === ''){
+                $('#error-message').html('<h2> Nothing Found</h2>');
+            } else {
+                let repoNum = responseJson.length;
+                let username = responseJson.login;
+                let avitar = responseJson.avatar_url;
+            }
+        })*/
